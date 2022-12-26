da77c8630387feac71728a3f0584f399d6d669d3
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-12-26 11:27:30 +0200

    up

---
+++ 81.manipulating-complex-objects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+let obj = {
+  "artist": "Metallica",
+    "title": "Master of Puppets",
+    "release_year": 1986,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+}
+myMusic.push(obj);
