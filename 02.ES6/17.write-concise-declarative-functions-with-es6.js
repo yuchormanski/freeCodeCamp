7e745d61427b0d3144c3abe5a9595791d46e28ff
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-12-28 10:09:16 +0200

    newFiles

---
+++ 02.ES6/17.write-concise-declarative-functions-with-es6.js
@@ -0,0 +1,12 @@
+// Refactor the function setGear inside the object bicycle to use the shorthand syntax
+
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
