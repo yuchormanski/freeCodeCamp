33b8787b0add2c8f21ebc68426507d834aee0f06
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-12-28 09:34:34 +0200

    up

---
+++ 02.ES6/15.create-strings-using-template-literals.js
@@ -0,0 +1,14 @@
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+  result.failure.forEach(el => failureItems.push(`<li class="text-warning">${el}</li>`))
+  // Only change code above this line
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
